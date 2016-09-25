31186c70794b0181badf452f864b6ee5b75b95dc
omemunama <xnursx@gmail.com>
2016-09-25 21:21:44 +0700

    transaction layout & fragment viewpager

---
+++ app/src/main/java/nursulaeman/catetduit/PagerAdapter.java
@@ -0,0 +1,37 @@
+package nursulaeman.catetduit;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by nur on 25/09/16.
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 0:
+                ExpensesFragment tab1 = new ExpensesFragment();
+                return tab1;
+            case 1:
+                IncomeFragment tab2 = new IncomeFragment();
+                return tab2;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
--- app/src/main/java/nursulaeman/catetduit/TransactionActivity.java
+++ app/src/main/java/nursulaeman/catetduit/TransactionActivity.java
@@ -1,5 +1,7 @@
 package nursulaeman.catetduit;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +11,36 @@ public class TransactionActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
-        this.setTitle("Transaction");
+/*
+        RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.layout_contact);
+        View view = getLayoutInflater().inflate(R.layout.activity_contact, mainLayout,false);
+        mainLayout.addView(view);
+*/
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("New Expense"));
+        tabLayout.addTab(tabLayout.newTab().setText("New Income"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
