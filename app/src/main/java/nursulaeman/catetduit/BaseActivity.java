8dffbb12439df40cd8ce917e426f8d2cd5a938b9
omemunama <xnursx@gmail.com>
2016-09-25 14:58:01 +0700

    set navigation & activity

---
+++ app/src/main/java/nursulaeman/catetduit/BaseActivity.java
@@ -0,0 +1,114 @@
+package nursulaeman.catetduit;
+
+/**
+ * Created by nur on 25/09/16.
+ */
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public abstract class BaseActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
+
+    private FrameLayout view_stub;
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Menu mDrawerMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_base);// The base layout that contains your navigation drawer.
+
+        view_stub = (FrameLayout) findViewById(R.id.view_stub);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDrawerMenu = mNavigationView.getMenu();
+        for(int i = 0; i < mDrawerMenu.size(); i++) {
+            mDrawerMenu.getItem(i).setOnMenuItemClickListener(this);
+        }
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        if (view_stub != null) {
+            LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            View stubView = inflater.inflate(layoutResID, view_stub, false);
+            view_stub.addView(stubView, lp);
+        }
+    }
+
+    @Override
+    public void setContentView(View view) {
+        if (view_stub != null) {
+            ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT);
+            view_stub.addView(view, lp);
+        }
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        if (view_stub != null) {
+            view_stub.addView(view, params);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_dashboard:
+                Intent lay_dash = new Intent(this, DashboardActivity.class);
+                startActivity(lay_dash);
+                break;
+            case R.id.nav_transaction:
+                Intent lay_trans = new Intent(this, TransactionActivity.class);
+                startActivity(lay_trans);
+                break;
+            case R.id.nav_syncronize:
+                Intent lay_sync = new Intent(this, SyncronizeActivity.class);
+                startActivity(lay_sync);
+                break;
+        }
+        return false;
+    }
+
+}
