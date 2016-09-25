4b1782e1b245ae28579b201f24afde860f71ac7a
omemunama <xnursx@gmail.com>
2016-09-25 16:02:51 +0700

    color & nav pic

--- app/src/main/java/nursulaeman/catetduit/BaseActivity.java
+++ app/src/main/java/nursulaeman/catetduit/BaseActivity.java
@@ -10,6 +10,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -36,6 +37,7 @@ public abstract class BaseActivity extends AppCompatActivity implements MenuItem
         mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(R.string.app_name);
 
         mDrawerMenu = mNavigationView.getMenu();
         for(int i = 0; i < mDrawerMenu.size(); i++) {
