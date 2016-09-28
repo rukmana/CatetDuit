167aa4edd8fb4ec9db38e8a1fb97700996cf84d0
omemunama <xnursx@gmail.com>
2016-09-28 11:43:22 +0700

    income transactoin retrofit GET

--- app/src/main/java/nursulaeman/catetduit/SyncronizeActivity.java
+++ app/src/main/java/nursulaeman/catetduit/SyncronizeActivity.java
@@ -2,13 +2,62 @@ package nursulaeman.catetduit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class SyncronizeActivity extends BaseActivity {
+
+    TextView tv_respond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_syncronize);
         this.setTitle("Syncronize");
+
+        tv_respond = (TextView) findViewById(R.id.tv_respond);
+
+        Button btn_sync = (Button) findViewById(R.id.btn_Sync);
+        btn_sync.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Gson gson = new GsonBuilder()
+                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                        .create();
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl("https://private-61259e-catetduit.apiary-mock.com/")
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build();
+
+                IncomeTransactionApi incometrans_api = retrofit.create(IncomeTransactionApi.class);
+
+                Call<IncomeTransactions> call = incometrans_api.getIncomeTransactions();
+                call.enqueue(new Callback<IncomeTransactions>() {
+
+                    @Override
+                    public void onResponse(Call<IncomeTransactions> call, Response<IncomeTransactions> response) {
+                        int status = response.code();
+                        tv_respond.setText(String.valueOf(status));
+                    }
+
+                    @Override
+                    public void onFailure(Call<IncomeTransactions> call, Throwable t) {
+                        tv_respond.setText(String.valueOf(t));
+                    }
+                });
+            }
+        });
     }
 }
